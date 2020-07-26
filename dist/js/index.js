535fa20419d4c578b1ef325556e8145b0c170e46
lenomosh <omondilenox@outlook.com>
2020-07-26 10:03:52 +0300

    refactrored js code

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,18 +1,4 @@
 $(document).ready(function () {
-//   $("#form").submit(function (event) {
-//     event.preventDefault();
-//     var name = $("input#b_name").val();
-//     var email = $("input#b_email").val();
-//     var message = $("textarea#b_comment").val();
-//     if ($("input#b_name").val() && $("input#b_email").val()) {
-//       alert(
-//         name +
-//           ", we have received your message. Thank you for reaching out to us."
-//       );
-//     } else {
-//       alert("Please enter your name and email!");
-//     }
-//   });
   $(".img-wrapper").click(function () {
     $(this).parent().children(".description").toggle(500);
   });
@@ -21,28 +7,31 @@ $(document).ready(function () {
   });
 });
 // Mailchimp inegration
-const submitForm = event=>{
-    event.preventDefault();
-    const form = new FormData(event.target);
-    const names = form.get('name').split(' ');
-    const FNAME = names[0]
-    const LNAME = names [1]
-    const EMAIL = form.get('email');
-    const JSONData = {FNAME,LNAME,EMAIL}
-    const url ='https://gmail.us17.list-manage.com/subscribe/post?u=ce559dfbfea8ab2302f8236cb&amp;id=4c3e72b3cb';
-    const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+const submitForm = (event) => {
+  event.preventDefault();
+  const form = new FormData(event.target);
+  const names = form.get("name").split(" ");
+  const FNAME = names[0];
+  const LNAME = names[1];
+  const EMAIL = form.get("email");
+  const JSONData = { FNAME, LNAME, EMAIL };
+  const url =
+    "https://gmail.us17.list-manage.com/subscribe/post?u=ce559dfbfea8ab2302f8236cb&amp;id=4c3e72b3cb";
+  const proxyUrl = "https://cors-anywhere.herokuapp.com/";
 
-    fetch( url,{
-        method:'POST',
-        body:JSON.stringify(JSONData),
-        headers:{
-            "Content-type": "application/json; charset=UTF-8"
-
-        }
-    }).then(res1 =>console.log(res1))
-    .then(data=>console.log(data))
-    .catch(error=>{
-        console.log(error);
-    })
-    alert(`Hey ${FNAME}, we have received your message. Thank you for reaching out to us.`)
-}
+  fetch(url, {
+    method: "POST",
+    body: JSON.stringify(JSONData),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((res1) => console.log(res1))
+    .then((data) => console.log(data))
+    .catch((error) => {
+      console.log(error);
+    });
+  alert(
+    `Hey ${FNAME}, we have received your message. Thank you for reaching out to us.`
+  );
+};
